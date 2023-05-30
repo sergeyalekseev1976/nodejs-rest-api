f416c0854543df3cb4e569178a2fbac71226046d
sergeyalekseev1976 <alekseev_ss@ukr.net>
2023-05-30 08:55:58 +0300

    add changes

--- helpers/schemas.js
+++ helpers/schemas.js
@@ -10,6 +10,6 @@ const updateSchema = Joi.object({
   name: Joi.string().min(3),
   email: Joi.string().email(),
   phone: Joi.string().min(6),
-});
+}).min(1);
 
 module.exports = { addSchema, updateSchema };
