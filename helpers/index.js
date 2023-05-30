4c97017f999ea535c44c9a170091fe7ca78abe21
sergeyalekseev1976 <alekseev_ss@ukr.net>
2023-05-30 08:55:34 +0300

    add variable

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const HttpError = require('./HttpError');
 const ctrlWrapper = require('./ctrlWrapper');
 const {addValid, updateValid} = require('./validate');
+const { addSchema, updateSchema } = require('./schemas');
 
-module.exports = {HttpError, ctrlWrapper, addValid, updateValid};
+module.exports = {HttpError, ctrlWrapper, addValid, updateValid, addSchema, updateSchema};
