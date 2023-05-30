8d9721bfa7f4883265985af5bb38b10cac329c46
sergeyalekseev1976 <alekseev_ss@ukr.net>
2023-05-30 09:00:15 +0300

    add changes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const ctrl = require('../../controllers/contacts');
+const { addValid, updateValid, addSchema, updateSchema} = require('../../helpers');
 const router = express.Router()
 
 router.get("/", ctrl.getAll);
 
 router.get("/:contactId", ctrl.getById);
 
-router.post("/", ctrl.addNew);
+router.post("/", addValid(addSchema), ctrl.addNew);
 
 router.delete("/:contactId", ctrl.deleteById);
 
-router.put("/:contactId", ctrl.updateById);
+router.put("/:contactId", updateValid(updateSchema), ctrl.updateById);
 
 module.exports = router;
