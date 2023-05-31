c5a598a39cf7c11206eeef97b363a81dfe02f019
sergeyalekseev1976 <alekseev_ss@ukr.net>
2023-05-31 12:14:24 +0300

    DB_HOST to process.env

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const app = require('./app')
-// wedckBfv3I8K9P7o
-const DB_HOST = "mongodb+srv://sergey:wedckBfv3I8K9P7o@cluster0.fz1h6cn.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+const {DB_HOST} = process.env
 mongoose.connect(DB_HOST)
 .then(() =>{
 app.listen(3000, () => {
