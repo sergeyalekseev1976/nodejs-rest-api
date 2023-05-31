0e839d264ead702a5a00d621fca299baebfd2463
sergeyalekseev1976 <alekseev_ss@ukr.net>
2023-05-31 11:18:24 +0300

    connect the database

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
+const app = require('./app')
+// wedckBfv3I8K9P7o
+const DB_HOST = "mongodb+srv://sergey:wedckBfv3I8K9P7o@cluster0.fz1h6cn.mongodb.net/db-contacts?retryWrites=true&w=majority"
+mongoose.connect(DB_HOST)
+.then(() =>{
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+  console.log("Database connection successful")
 })
+})
+.catch(error => {
+  console.log(error.massage);
+  process.exit(1);
+})
+
