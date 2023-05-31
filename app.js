06bd74bd52a9897235cf67a7f25149bf059d7136
sergeyalekseev1976 <alekseev_ss@ukr.net>
2023-05-31 12:39:47 +0300

    removing confidential data

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
 
 app.use('/api/contacts', contactsRouter)
 
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const app = require('./app')
 
-const {DB_HOST} = process.env
+const {DB_HOST} = process.env;
+const {PORT} = process.env;
+
 mongoose.connect(DB_HOST)
 .then(() =>{
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Database connection successful")
 })
 })
